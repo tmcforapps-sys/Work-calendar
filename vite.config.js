9835e6896ca0f73b2bcfecaf834b634af8e895b5
tmcforapps-sys <tmcforapps@gmail.com>
2025-09-12 11:48:24 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  root: ".", // project root
-  publicDir: "public", // static files เช่น favicon, index.html
+  root: ".", // root เป็น project root
+  publicDir: "public", // public folder
   build: {
     outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, "public/index.html"),
+      input: path.resolve(__dirname, "public/index.html"), // ใช้ index.html จาก public
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // import JS/CSS จาก src
+      "@": path.resolve(__dirname, "src"), // สำหรับ import JS/CSS
     },
   },
 });
